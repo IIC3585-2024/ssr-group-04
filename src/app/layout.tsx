1ee8ea25f309929aafbc8ef04c9320130e8874d9
domingo2000 <dedwards@buk.cl>
2024-06-23 17:16:43 -0400

    feat: added bottom navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./navbar.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <nav>
+          <div>
+            <a href="/">Home</a>
+          </div>
+          <div>
+            <a href="/shows">Shows</a>
+          </div>
+        </nav>
+      </body>
     </html>
   );
 }
